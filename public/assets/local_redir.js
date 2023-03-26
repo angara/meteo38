a2b11c09c6211ae0d1cc60d46a7019ce8977d7de
Maxim Penzin <maxp@ya.ru>
2023-03-26 13:25:27 +0800

    update default st_list

--- public/assets/local_redir.js
+++ public/assets/local_redir.js
@@ -1,6 +1,6 @@
 var st_list = window.localStorage.getItem("st_list");
 if(!st_list) { 
-  st_list = "irgp,soln,uiii,istok,npsd,olha,khomutovo,mamai_sphera"; 
+  st_list = "irgp,soln,uiii,npsd,istok,olha,khomutovo,khrust_park,mamai_sphera"; 
   window.localStorage.setItem("st_list",st_list); 
 };
 window.location.assign( window.location.protocol+"//"+window.location.host+location.pathname+"?st_list="+st_list ); 
